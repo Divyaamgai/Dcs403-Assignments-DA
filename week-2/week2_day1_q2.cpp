b8e61888a153d9ca40edf386f3d3a5b8d139e8ec
Divya Amgai <divyaamgai@Divyas-MacBook-Pro.local>
2026-01-19 08:11:21 +0545

    Week 2 Assignment

--- week-2/week2_day1_q2.cpp
+++ week-2/week2_day1_q2.cpp
@@ -4,16 +4,12 @@ using namespace std;
 int main() {
     int a, b;
 
-    cout << "Enter first number: ";
-    cin >> a;
-
-    cout << "Enter second number: ";
-    cin >> b;
+    cin >> a >> b;
 
     if (a > b) {
-        cout << a << " is greater";
+        cout << a << " is greater.";
     } else {
-        cout << b << " is greater";
+        cout << b << " is greater.";
     }
 
     return 0;
